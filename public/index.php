89e2bc7f4f62e2421fa9ab139ace1bca3965c96f
Peto.B000 <peto.b000@gmail.com>
2025-01-13 00:44:42 +0100

    project finished

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 // CORS Headers
-header('Access-Control-Allow-Origin: http://localhost:5173');
+header('Access-Control-Allow-Origin: https://petob000.github.io/job_test_FE');
 header('Access-Control-Allow-Methods: POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 
